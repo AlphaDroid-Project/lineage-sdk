Poison-Fang/android_lineage-sdk

Battery light: 100% charged level (3/3)
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

sdk/src/java/org/lineageos/internal/notification/LineageBatteryLights.java
@@ -55,6 +55,7 @@ public final class LineageBatteryLights {
     private int mBatteryLowARGB;
     private int mBatteryMediumARGB;
     private int mBatteryFullARGB;
+    private int mBatteryReallyFullARGB;
     private int mBatteryBrightnessLevel;
     private int mBatteryBrightnessZenLevel;
 
@@ -166,9 +167,15 @@ public final class LineageBatteryLights {
         } else if (status == BatteryManager.BATTERY_STATUS_CHARGING
                 || status == BatteryManager.BATTERY_STATUS_FULL) {
             if (status == BatteryManager.BATTERY_STATUS_FULL || level >= 90) {
-                // Battery is full or charging and nearly full.
-                ledValues.setColor(mBatteryFullARGB);
-                ledValues.setSolid();
+                if (level == 100) {
+                    // Battery is full
+                    ledValues.setColor(mBatteryReallyFullARGB);
+                    ledValues.setSolid();
+                } else {
+                    // Battery is nearly full
+                    ledValues.setColor(mBatteryFullARGB);
+                    ledValues.setSolid();
+                }
             } else {
                 // Battery is charging and not nearly full.
                 ledValues.setColor(mBatteryMediumARGB);
@@ -226,6 +233,9 @@ public final class LineageBatteryLights {
                 resolver.registerContentObserver(LineageSettings.System.getUriFor(
                         LineageSettings.System.BATTERY_LIGHT_FULL_COLOR), false, this,
                         UserHandle.USER_ALL);
+                resolver.registerContentObserver(LineageSettings.System.getUriFor(
+                        LineageSettings.System.BATTERY_LIGHT_REALLY_FULL_COLOR), false, this,
+                        UserHandle.USER_ALL);
             }
 
             if (mCanAdjustBrightness) {
@@ -273,6 +283,10 @@ public final class LineageBatteryLights {
             mBatteryFullARGB = LineageSettings.System.getIntForUser(resolver,
                     LineageSettings.System.BATTERY_LIGHT_FULL_COLOR, res.getInteger(
                     com.android.internal.R.integer.config_notificationsBatteryFullARGB),
+                    UserHandle.USER_CURRENT);
+            mBatteryReallyFullARGB = LineageSettings.System.getIntForUser(resolver,
+                    LineageSettings.System.BATTERY_LIGHT_REALLY_FULL_COLOR, res.getInteger(
+                    com.android.internal.R.integer.config_notificationsBatteryReallyFullARGB),
                     UserHandle.USER_CURRENT);
 
             // Adustable battery LED brightness.

AlphaDroid-Project/lineage-sdk

core/java/org/lineageos/internal/notification/LineageBatteryLights.java
@@ -43,6 +43,7 @@ public final class LineageBatteryLights {
     private int mBatteryLowARGB;
     private int mBatteryMediumARGB;
     private int mBatteryFullARGB;
+    private int mBatteryReallyFullARGB;
     private int mBatteryBrightnessLevel;
     private int mBatteryBrightnessZenLevel;
 
@@ -158,9 +159,15 @@ public final class LineageBatteryLights {
             }
         } else if (isChargingOrFull) {
             if (isCharged || level >= 90) {
-                // Battery is full or charging and nearly full.
-                ledValues.setColor(mBatteryFullARGB);
-                ledValues.setSolid();
+                if (level == 100) {
+                    // Battery is full
+                    ledValues.setColor(mBatteryReallyFullARGB);
+                    ledValues.setSolid();
+                } else {
+                    // Battery is nearly full
+                    ledValues.setColor(mBatteryFullARGB);
+                    ledValues.setSolid();
+                }
             } else {
                 // Battery is charging and not nearly full.
                 ledValues.setColor(mBatteryMediumARGB);
@@ -231,6 +238,9 @@ public final class LineageBatteryLights {
                 resolver.registerContentObserver(Settings.System.getUriFor(
                         Settings.System.BATTERY_LIGHT_FULL_COLOR), false, this,
                         UserHandle.USER_ALL);
+                resolver.registerContentObserver(Settings.System.getUriFor(
+                        Settings.System.BATTERY_LIGHT_REALLY_FULL_COLOR), false, this,
+                        UserHandle.USER_ALL);
             }
 
             if (mCanAdjustBrightness) {
@@ -283,6 +293,10 @@ public final class LineageBatteryLights {
             mBatteryFullARGB = Settings.System.getIntForUser(resolver,
                     Settings.System.BATTERY_LIGHT_FULL_COLOR, res.getInteger(
                     com.android.internal.R.integer.config_notificationsBatteryFullARGB),
+                    UserHandle.USER_CURRENT);
+            mBatteryReallyFullARGB = Settings.System.getIntForUser(resolver,
+                    Settings.System.BATTERY_LIGHT_REALLY_FULL_COLOR, res.getInteger(
+                    com.android.internal.R.integer.config_notificationsBatteryReallyFullARGB),
                     UserHandle.USER_CURRENT);
 
             // Adjustable battery LED brightness.